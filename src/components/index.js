e7ea4fc371ae1fb810916c5eeb28da80e3c2949a
Erudite885 <erudite885@gmail.com>
2023-12-24 22:22:16 +1300

    starting files

---
+++ src/components/index.js
@@ -0,0 +1,23 @@
+import About from './About/About';
+import CTA from "./CTA/CTA";
+import Footer from './Footer/Footer';    
+import Hero from './Hero/Hero';
+import Journey from './Journey/Journey'; 
+import Mission from "./Mission/Mission";
+import Navbar from './Navbar/Navbar';    
+import Partners from './Partners/Partners';
+import Press from './Press/Press';
+import Trusted from './Trusted/Trusted';
+
+export {
+    Navbar,
+    About,
+    CTA,
+    Footer,
+    Hero,
+    Journey,
+    Mission,
+    Partners,
+    Press,
+    Trusted
+}
